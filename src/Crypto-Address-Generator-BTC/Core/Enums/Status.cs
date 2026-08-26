424bb15bd4a97d2bdf7209e307348cd4fbcb20c5
nexuskazekagehum <karenedwardsi557@gmail.com>
2026-08-26 12:40:21 -0700

    Initial commit: src/Crypto-Address-Generator-BTC/Core/Enums/Status.cs

---
+++ src/Crypto-Address-Generator-BTC/Core/Enums/Status.cs
@@ -0,0 +1,13 @@
+namespace CryptoAddressGeneratorBTC.Core.Enums
+{
+    public enum Status
+    {
+        Unknown = 0,
+        Pending = 1,
+        Processing = 2,
+        Succeeded = 3,
+        Failed = 4,
+        Cancelled = 5,
+        Skipped = 6
+    }
+}
